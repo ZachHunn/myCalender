400e3e4261521b7018074775905c563bce2761a6
ZachHunn <43091706+ZachHunn@users.noreply.github.com>
2020-09-02 19:27:51 -0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myCalender.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
